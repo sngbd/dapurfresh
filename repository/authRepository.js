9846abf36d18c7ac27ede861c6f9e6259a472153
syaiful <syaifulmaulana817@gmail.com>
2022-10-17 15:49:42 +0700

    cek

--- repository/authRepository.js
+++ repository/authRepository.js
@@ -4,23 +4,23 @@ const { Op } = require('sequelize');
 const createUser = async (user) => {
   if (user.ref_code_friend) {
     const refCodeFriend = await User.findOne({
-      where: { ref_code: user.ref_code_friend }
+      where: { ref_code: user.ref_code_friend },
     });
-    
+
     if (!refCodeFriend) {
-      throw 'friend\'s referral code is invalid';
+      throw "friend's referral code is invalid";
     }
   }
 
   const [newUser, created] = await User.findOrCreate({
-    where: { username: user.username},
+    where: { username: user.username },
     defaults: user,
   });
-  
+
   if (created) return newUser;
   throw 'username already exists';
 };
 
 module.exports = {
   createUser,
-}
+};
