55e5d3b119f520ec2b2591a1fc72af9971a4ef6b
Gilberdi Sinaga <gilberdising012@gmail.com>
2022-10-25 14:19:55 +0700

    fix: jwt error

--- middlewares/validation/validateToken.js
+++ middlewares/validation/validateToken.js
@@ -1,15 +1,18 @@
 const jwt = require('jsonwebtoken');
 
 const validateToken = async (req, res, next) => {
-    const authorizationHeader = req.headers.authorization;
-    const accessToken = authorizationHeader && authorizationHeader.split(' ')[1];
-    if (accessToken == null) 
-        return res.notAuthorized();
+  const authorizationHeader = req.headers.authorization;
+  const accessToken = authorizationHeader && authorizationHeader.split(' ')[1];
+  if (accessToken == null) return res.notAuthorized();
+  try {
     jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.forbidden();
-        req.user = user;
+      if (err) throw err;
+      req.user = user;
     });
-    return next();
+    next();
+  } catch (err) {
+    return res.forbidden(err.message);
+  }
 }
 
 module.exports = validateToken;
