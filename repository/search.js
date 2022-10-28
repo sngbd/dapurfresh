d58a82d1ffb841cba921a40ce59b8c8ef18bb0cc
nthnlius <nthnlius@gmail.com>
2022-10-28 10:26:51 +0700

    FEAT search : create : 	repository/search.js Problem : 	can't join table. no foreign key declared

---
+++ repository/search.js
@@ -0,0 +1,27 @@
+const { Product } = require('../models');
+const { Order } = require('../models');
+const { Order_Item } = require('../models');
+const getResult = () => {
+  const get = Product.findAll(
+    {
+      attributes: ['product_id, title'],
+      where: {
+        [Op.and]: [
+
+        ]
+      }
+    }
+  )
+}
+/* Querynya :
+  SELECT p.product_id, title
+  FROM "Order_Items" as oi join "Orders" as o on oi.order_id = o.id JOIN "Products" p on oi.product_id = p.id
+  WHERE Status LIKE 'Proses' OR status LIKE 'Selesai' GROUP BY product_id ORDER BY COUNT(product_id) ;
+ 
+ATAU 
+
+  SELECT * from "Products" where "Products".id in (	SELECT product_id from "Order_Items" as oi JOIN "Orders" as o ON o.id = oi.order_id
+  WHERE Status = 'Proses' OR Status = 'Selesai'
+  GROUP BY product_id
+  ORDER By COUNT(product_id) desc);
+ */
