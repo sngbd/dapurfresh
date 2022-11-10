7d927c773f70231500e223f172da86bd03fb7b5f
syaiful <syaifulmaulana817@gmail.com>
2022-11-10 11:14:40 +0700

    add seed help

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, DB_DIALECT, DB_TEST_USERNAME, DB_TEST_PASSWORD, DB_TEST_NAME, DB_TEST_HOST, DB_TEST_DIALECT, DATABASE_URL } = process.env;
+const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, DB_DIALECT, DB_TEST_USERNAME, DB_TEST_PASSWORD, DB_TEST_NAME, DB_TEST_HOST, DB_TEST_DIALECT } = process.env;
 
 module.exports = {
   development: {
@@ -10,14 +10,14 @@ module.exports = {
     host: DB_HOST,
     dialect: DB_DIALECT,
     // Use a different storage. Default: none
-    // seederStorage: 'json',
+    seederStorage: 'json',
     // Use a different file name. Default: sequelize-data.json
-    // seederStoragePath: 'sequelize-data.json',
+    seederStoragePath: 'sequelize-data.json',
 
     // Use a different storage type. Default: sequelize
-    // migrationStorage: 'json',
+    migrationStorage: 'json',
     // Use a different file name. Default: sequelize-meta.json
-    // migrationStoragePath: 'sequelize-meta.json',
+    migrationStoragePath: 'sequelize-meta.json',
   },
   test: {
     username: DB_TEST_USERNAME,
@@ -36,7 +36,7 @@ module.exports = {
     migrationStoragePath: 'sequelize-meta.json',
   },
   production: {
-    use_env_variable: DATABASE_URL,
+    use_env_variable: 'DATABASE_URL',
     dialect: DB_DIALECT,
     dialectOptions: {
       ssl: {
---
+++ seeders/20221110040634-help-seed.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      'Help',
+      [
+        {
+          title: 'Cara Memesan Product',
+          description:
+            '1. Cari Produk yang kamu inginkan <br> 2. Tambahkan aja produknya dengan menekan tombol tambah <br> 3. Tekan keranjang yang ada dibawah layar kamu untuk masuk ke halaman checkout <br> 4. Pastikan data-data kamu valid yaa. Jika ada produk yang tidak ditemukan, kamu bisa tambahin di Catatan Tambahan <br> 5. Terakhir, tombol pesan dehh',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
